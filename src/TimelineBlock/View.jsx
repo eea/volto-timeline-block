bfcf52143f656b908f12b3374cc3d596b35fc1f7
Miu Razvan <miu.razvan28@gmail.com>
2022-03-23 16:13:36 +0200

    don't use timeline from volto-eea-design-system

--- src/TimelineBlock/View.jsx
+++ src/TimelineBlock/View.jsx
@@ -1,7 +1,47 @@
 import React from 'react';
 import { compose } from 'redux';
+import cx from 'classnames';
+import { Grid, Card, Label, Icon } from 'semantic-ui-react';
 import { injectLazyLibs } from '@plone/volto/helpers/Loadable/Loadable';
-import { Timeline } from '@eeacms/volto-eea-design-system/ui';
+function Timeline({ children, reversed }) {
+  return (
+    <div className="eea timeline">
+      <Grid className={cx({ 'any reversed': reversed })}>
+        <Grid.Row>{children}</Grid.Row>
+      </Grid>
+    </div>
+  );
+}
+
+Timeline.Content = (args) => (
+  <Grid.Column width={10}>
+    <Card fluid raised color={args.color}>
+      <Card.Content>
+        <Label
+          pointing={args.pointing}
+          attached="top"
+          style={{ marginLeft: '0' }}
+          color={args.color}
+        >
+          {args.time}
+        </Label>
+        <Card.Header>{args.title}</Card.Header>
+        <Card.Description>{args.description}</Card.Description>
+        {/* <Label.Group>
+          {tags.map((tag, i) => (
+            <Label key={i.toString()}>{tag}</Label>
+          ))}
+        </Label.Group> */}
+      </Card.Content>
+    </Card>
+  </Grid.Column>
+);
+
+Timeline.Icon = (args) => (
+  <Grid.Column width={2} className="line">
+    <Icon name={args.icon} color={args.color} inverted circular />
+  </Grid.Column>
+);
 
 const View = ({ data, mode, moment }) => {
   const { items = [], reversed, hideTime } = data;
